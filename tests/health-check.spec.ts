330cee07e3d4ad6d26cf40823d5d2476b306b31c
Gustavo Gonçalves <gustavoprof3@gmail.com>
2022-07-27 21:23:03 -0300

    Delete tests directory

--- tests/health-check.spec.ts
+++
@@ -1,9 +0,0 @@
-import '../../infra/dotenv';
-import axios from 'axios';
-
-describe('health-check', () => {
-  test('/ request', async () => {
-    const response = await axios.get(`http://localhost:${process.env.PORT}/`);
-    expect(response.data.ok).toBeTruthy();
-  });
-});
